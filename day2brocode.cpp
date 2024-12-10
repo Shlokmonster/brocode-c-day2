e95980eab252d61d5444b47c8dfe170b837f031d
SHLOK KADAM <shlokkadam46@gmail.com>
2024-12-10 16:24:26 +0530

    Add files via upload
    it is day 2 of bro code

---
+++ day2brocode.cpp
@@ -0,0 +1,41 @@
+// ternary operator
+// # include<iostream>
+
+// int main(){
+// ternary operator ?; = replacement of an if/else statement 
+// condition ? expression1 :expression2;
+
+// pass or fail programm 
+// int grade =55;
+// (grade>=60) ? //this is how the condition is written
+//     std::cout<<"pass" : std::cout<<"fail";
+
+//to check even or odd number
+
+// int num1;
+// std::cout<<"enter the number";
+// std::cin>>num1;
+// (num1%2==0)? std::cout<<"it is an even number":std::cout<<"It is an odd number";
+
+
+// boolean variable
+
+// bool hungry = false;
+// (hungry)?std::cout<<"YOU HAD LUNCH":std::cout<<"YOUR HUNGRY";
+// return 0;
+// }
+
+
+// # include <iostream>
+
+// int main()
+// {
+
+//     // && = check if two conditions are true
+//     // || = check if atleast one of the two conditions is true
+//     //  ! = reverse the logical state of its operand
+
+
+// }
+
+
